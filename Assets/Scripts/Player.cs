ed4c4910f4eb047cc1b285254f48bfcbe5a276d7
TuanAnh <tuan55293@gmail.com>
2022-08-01 18:51:11 +0700

    update game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 jumpForce;
+    public Vector2 jumpForceUp;
+    public float minForceX;
+    public float maxForceX;
+    public float minForceY;
+    public float maxForceY;
+
+    [HideInInspector]
+    public int lastPlatformId;
+    bool m_Jumped;
+    bool m_powerSetted;
+
+    Rigidbody2D m_rb;
+    Animator m_anim;
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        SetPower();
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetPower(true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            SetPower(false);
+        }
+    }
+    void SetPower()
+    {
+        if(m_powerSetted && !m_Jumped)
+        {
+            jumpForce.x += jumpForceUp.x * Time.deltaTime;
+            jumpForce.y += jumpForceUp.y * Time.deltaTime;
+
+            jumpForce.x = Mathf.Clamp(jumpForce.x, minForceX, maxForceX);
+            jumpForce.y = Mathf.Clamp(jumpForce.y, minForceY, maxForceY);
+        }
+    }
+
+    public void SetPower(bool isHoldingMouse)
+    {
+        m_powerSetted = isHoldingMouse;
+
+        if(!m_powerSetted && !m_Jumped)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if(!m_rb || jumpForce.x <=0 || jumpForce.y <= 0)
+        {
+            return;
+        }
+        m_rb.velocity = jumpForce;
+        m_Jumped = true;
+
+        if (m_anim)
+        {
+            m_anim.SetBool("Jumped", true);
+        }
+    }
+}
---
+++ Assets/Scripts/PrefConst.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefConst
+{
+    public const string BEST_SCORE = "best_score";
+}
